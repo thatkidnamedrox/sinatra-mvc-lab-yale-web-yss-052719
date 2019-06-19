5e91613206ab172a5c2a378631a09457c30d42c7
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-19 06:53:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,13 +4,11 @@ class PigLatinizer
   end
 
   def piglatinize(string)
-    # f = {
-    #   vowel: lambda {|c| c =~ /[aeiou]/ && c.is_a?(String)}
-    # }
+    f = {vowel: lambda {|c| c =~ /[aeiou]/ && c.is_a?(String)}}
     words = string.split(" ")
     words.map do |word|
-      if !f[:vowel](word[0])
-        if f[:vowel](word[1])
+      if !vowel(word[0])
+        if vowel(word[1])
           word[1..word.size].concat(word[0].concat("ay"))
         else
           word[2..word.size].concat(word[0..2].concat("ay"))
