0957426d3b7fd0f38a5551924eb9e97012f60158
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-19 06:40:47 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,9 +4,15 @@ class PigLatinizer
   end
 
   def piglatinize(string)
+    f = {
+      vowel?: lambda {|c| c =~ /[aeiou]/}
+    }
     string.split(" ")
 
-    string.join(" ")
+    stirng.map do |word|
+      
+    end
+    # string.join(" ")
   end
 
 end
