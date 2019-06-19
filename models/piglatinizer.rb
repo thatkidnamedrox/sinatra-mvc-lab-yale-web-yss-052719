f5829201f443e79923bf22db8580eb9da755a80e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-19 06:45:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,8 +9,8 @@ class PigLatinizer
 
     stirng.map do |word|
       if !vowel(word[0])
-        
-        if vowel(word[1]) 
+
+        if vowel(word[1])
           word[1..word.size].concat(word[0].concat("ay"))
         else
           word[2..word.size]
